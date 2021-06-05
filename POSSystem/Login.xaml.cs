02459cc596be573cf150fd67fb1fa3b3002482e4
Leptop <pspcs@DESKTOP-I7ULMPC>
2021-06-05 13:59:19 +0530

    Login Code

--- POSSystem/Login.xaml.cs
+++ POSSystem/Login.xaml.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace POSSystem
 {
@@ -19,9 +11,50 @@ namespace POSSystem
     /// </summary>
     public partial class Login : Window
     {
+        string conString = "";
         public Login()
         {
             InitializeComponent();
+        }
+
+        private void btnclick(object sender, RoutedEventArgs e)
+        {
+            string number = (sender as Button).Content.ToString();
+            TxtPassword.Text += number;
+        }
+
+        private void TxtClear_Click(object sender, RoutedEventArgs e)
+        {
+            TxtPassword.Text = "";
+        }
+
+        private void TxtSignIn_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection con = new SqlConnection(conString);
+            string query = "select * from userregi where password=@password ";
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            cmd.Parameters.AddWithValue("@password", TxtPassword.Text);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+
+            if (dt.Rows.Count > 0)
+            {
+                this.Hide();
+                string username = dt.Rows[0]["UserName"].ToString();
+                //Mainpage frm = new Mainpage(username);
+                //frm.Show();
+                //this.Hide();
+
+            }
+            else
+            {
+                MessageBox.Show("Please Enter Correct Password");
+            }
         }
     }
 }
