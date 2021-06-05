b61577db7c2a585ffe5c3405ba78fd68303c2368
Pinakin <pinakin.p905@gmail.com>
2021-06-05 13:59:17 +0530

    Create User Code

--- POSSystem/CreateUser.xaml.cs
+++ POSSystem/CreateUser.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,43 @@ namespace POSSystem
     /// </summary>
     public partial class CreateUser : Window
     {
+        string constring = "Server=184.168.194.64;Database=db_POS; User ID=pinakin;Password=PO$123456; Trusted_Connection=false;MultipleActiveResultSets=true";
         public CreateUser()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection con = new SqlConnection(constring);
+            string queryS = "Select UserName,PassWord from UserRegi where UserName=@userName or Password=@password";
+            SqlCommand cmd = new SqlCommand(queryS, con);
+            cmd.Parameters.AddWithValue("@userName", txtUser.Text);
+            cmd.Parameters.AddWithValue("@password", txtPassword.Text);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+            if (dt.Rows.Count > 0)
+            {
+                MessageBox.Show("UserName Or Password Already Exist!");
+            }
+            else
+            {
+                string time = DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss tt");
+                string queryI = "Insert into UserRegi(UserName,Password,CreateOn)Values(@userName,@password,@time)";
+                SqlCommand cmdI = new SqlCommand(queryI, con);
+                cmdI.Parameters.AddWithValue("@userName", txtUser.Text);
+                cmdI.Parameters.AddWithValue("@password", txtPassword.Text);
+                cmdI.Parameters.AddWithValue("@time", time);
+                con.Open();
+                cmdI.ExecuteNonQuery();
+                con.Close();
+                txtPassword.Text = "";
+                txtUser.Text = "";
+            }
         }
     }
 }
