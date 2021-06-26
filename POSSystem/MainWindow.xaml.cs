e41bb0de71938b4eedfda8ac51a5a107dd7c6c03
PSPAdmin <pspcstore@gmail.com>
2021-06-26 17:37:50 +0530

    create saleitem report

--- POSSystem/MainWindow.xaml.cs
+++ POSSystem/MainWindow.xaml.cs
@@ -760,5 +760,50 @@ namespace POSSystem
             }
             catch (Exception ex) { }
         }
+
+        public static void SendErrorToText(Exception ex, string errorFileName)
+        {
+            var line = Environment.NewLine + Environment.NewLine;
+            ErrorlineNo = ex.StackTrace.Substring(ex.StackTrace.Length - 7, 7);
+            Errormsg = ex.GetType().Name.ToString();
+            extype = ex.GetType().ToString();
+
+            ErrorLocation = ex.Message.ToString();
+            try
+            {
+                string filepath = System.AppDomain.CurrentDomain.BaseDirectory;
+                string errorpath = filepath + "\\ErrorFiles\\";
+                if (!Directory.Exists(errorpath))
+                {
+                    Directory.CreateDirectory(errorpath);
+                }
+
+                if (!Directory.Exists(filepath))
+                {
+                    Directory.CreateDirectory(filepath);
+                }
+                filepath = filepath + "log.txt";   //Text File Name
+                if (!File.Exists(filepath))
+                {
+                    File.Create(filepath).Dispose();
+                }
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    string error = "Log Written Date:" + " " + DateTime.Now.ToString() + line + "File Name :" + errorFileName + line + "Error Line No :" + " " + ErrorlineNo + line + "Error Message:" + " " + Errormsg + line + "Exception Type:" + " " + extype + line + "Error Location :" + " " + ErrorLocation + line + " Error Page Url:" + " " + exurl + line + "User Host IP:" + " " + hostIp + line;
+                    sw.WriteLine("-----------Exception Details on " + " " + DateTime.Now.ToString() + "-----------------");
+                    sw.WriteLine("-------------------------------------------------------------------------------------");
+                    sw.WriteLine(line);
+                    sw.WriteLine(error);
+                    sw.WriteLine("--------------------------------*End*------------------------------------------");
+                    sw.WriteLine(line);
+                    sw.Flush();
+                    sw.Close();
+
+                }
+            }
+            catch (Exception e)
+            {
+            }
+        }
     }
 }
